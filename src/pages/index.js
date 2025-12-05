ffa717c3de1e030ffe42b8de3083c6ce1db8e23f
AlvarezJhonn-FS <JSAlvarezMolina@student.fullsail.edu>
2025-12-05 06:15:07 -0500

    added the header component to the index file

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Header from "../components/Header";
+
+
+const Index = () => {
+  return (
+<div className="index">
+    <Header/>
+  <h1>Welcome to my site</h1>
+</div>
+  )
+}
+
+export default Index;
+
+const index = {
+  color: "#3d2192ff",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+}
